407ca80337b5910b24d563a8ab80565f0c7fa102
gmgiglio <gmgiglio@uc.cl>
2018-10-15 21:03:41 -0300

    algoritmo D'Hondt implementado
    Al apretar botón paso da un paso en el algoritmo

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ var chart = d3.select(".chart")
     .attr("width", width)
     .attr("height", height);
 
-d3.tsv("data.tsv", type, function(error, data) {
-  
+var Data = [];
+d3.tsv("data.tsv", type, function(error, data) { 
   y.domain([0, d3.max(data, function(d) { return d.value; })]);
     
   data.forEach(function(element){
       element.divNum = 3;
   });
-    
+  
   var barWidth = width / data.length;
 
   var bar = chart.selectAll("g")
@@ -29,12 +29,16 @@ d3.tsv("data.tsv", type, function(error, data) {
     .attr("width", barWidth - 1)
     .attr("class", "total-bar");
     
+
   bar.append("rect")
       .attr("y", function(d) { return height - ((height - y(d.value)) / d.divNum); })
       .attr("height", function(d) { return (height - y(d.value)) / d.divNum ; })
       .attr("width", barWidth - 1)
       .attr("class", "remaining-bar")
       .style("fill", function(d) { return d.color; });
+      
+  
+  Data = data;
 
 });
 
@@ -42,3 +46,43 @@ function type(d) {
   d.value = +d.value; // coerce to number
   return d;
 }
+
+
+var paso = 1;
+
+function votosActuales(partido){
+  return partido.value / partido.divNum;
+}
+
+function pasoDHondt(){
+  
+  d3.select("#printeo").html(paso);
+  paso++;
+  
+  let maxd = Data.reduce((max,d) => votosActuales(max) > votosActuales(d) ? max : d);
+  
+  ganaCupo(maxd);
+  maxd.divNum += 1;
+  
+  chart.selectAll("g")
+    .selectAll(".remaining-bar")
+    .attr("y", function(d) { return height - ((height - y(d.value)) / d.divNum); })
+    .attr("height", function(d) { return (height - y(d.value)) / d.divNum ; });
+   
+}
+
+
+function ganaCupo(pardido){
+  //IMPLEMENTAR
+}
+
+function imprimirValores(){
+  d3.select("#printeo").selectAll("p")
+    .data(Data)
+  .enter()
+    .append("p")
+    .text(function(d){
+      return d.value;
+});
+}
+
